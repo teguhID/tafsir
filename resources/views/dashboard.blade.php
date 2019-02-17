9055aadc6398cb40b2674b6cd1c7f6310ac21a6f
teguhID <teguh.nugraha7@gmail.com>
2019-02-17 10:11:49 +0700

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-Dashboard</br></br>
+Dashboard Tafsir Al-Quran </br></br>
 <a href="{{ url('/ayat') }}">Ayat</a></br>
 <a href="{{ url('/ibnuKatsir') }}">Ibnu Katsir</a></br>
 <a href="#">Jalalain</a></br>
